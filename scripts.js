5e39148ffc119dfd8a24bf51fc1ed6f7bd25a2ca
rajeshdn <codingmarket@gmail.com>
2020-01-21 19:53:20 +0530

    files added

---
+++ scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+$(".size_select").click(function() {
+  $(".sliders").addClass("active");
+  $(".select_size").show();
+  $(".select_quantity").hide();
+});
+
+$(".qty_select").click(function() {
+  $(".sliders").addClass("active");
+  $(".select_size").hide();
+  $(".select_quantity").show();
+});
+
+$(".sliders .close").click(function() {
+  $(".sliders").removeClass("active");
+});
+
+$(".sliders .select_size .body ul li").click(function() {
+  $(".sliders .select_size .body ul li").removeClass("active");
+  $(this).addClass("active");
+  $(".size_select span").text($(this).text());
+  $(".sliders").removeClass("active");
+});
+
+$(".sliders .select_quantity .body ul li").click(function() {
+  $(".sliders .select_quantity .body ul li").removeClass("active");
+  $(this).addClass("active");
+  $(".qty_select span").text($(this).text());
+  $(".sliders").removeClass("active");
+});
+
+})
